e93038ebd3bc1d434596eebef4ceb30ae8547e51
genialtechie <only1hbo@icloud.com>
2023-12-30 01:59:12 -0500

    fixed ref err

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const VideoComponent = () => {
-  const isMobile = window?.innerWidth < 768;
+  const isMobile = typeof window !== 'undefined' && window.innerWidth < 768;
   return (
     <div className="contrast-50 saturate-50">
       {isMobile ? (
@@ -18,7 +18,7 @@ export const VideoComponent = () => {
         ></video>
       ) : (
         <video
-          src="https://res.cloudinary.com/dpti4rlt1/video/upload/f_auto:video,q_auto/v1/sme/sk7brbbgl7daok4a32ul"
+          src="https://res.cloudinary.com/dpti4rlt1/video/upload/f_auto:video,q_auto/v1/sme/sk7brbbgl7daok4a32ulconst isMobile = typeof window !== 'undefined' && window.innerWidth < 768;const isMobile = typeof window !== 'undefined' && window.innerWidth < 768;"
           width={window?.innerWidth}
           height={window?.innerHeight}
           autoPlay
